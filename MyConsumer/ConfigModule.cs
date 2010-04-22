e10752f9e3895648553e6881d3e73816a4f9afe4
unknown <joliver@.(none)>
2010-04-22 14:38:09 -0600

    Moved configuration wireup to different project.

--- MyConsumer/ConfigModule.cs
+++ MyConsumer/ConfigModule.cs
@@ -1,11 +1,8 @@
 namespace MyConsumer
 {
-	using System;
 	using System.Transactions;
 	using Autofac.Builder;
 	using IdempotentConsumer;
-	using IdempotentConsumer.Core;
-	using NServiceBus;
 	using NServiceBus.MessageSinks;
 
 	public class ConfigModule : Module
@@ -19,45 +16,6 @@ namespace MyConsumer
 					new TransactionScope(), c.Resolve<ITrackUnitOfWork>()))
 				.As<IMessageSink>()
 				.ContainerScoped();
-
-			builder
-				.Register(c => new DuplicateMessageFilter(c.Resolve<ILoadMessages>(), c.Resolve<IDispatchMessages>()))
-				.As<IFilterDuplicateMessages>()
-				.ContainerScoped()
-				.ExternallyOwned();
-
-			builder
-				.Register(c => new IdempotentUnitOfWork(
-					c.Resolve<IStoreMessages>(), c.Resolve<IDispatchMessages>(), c.Resolve<IBuildMessages>()))
-				.As<ITrackUnitOfWork>()
-				.ContainerScoped()
-				.ExternallyOwned();
-
-			builder
-				.Register(c => new InMemoryStorage())
-				.As<IStoreMessages>()
-				.As<ILoadMessages>()
-				.SingletonScoped()
-				.ExternallyOwned();
-
-			builder
-				.Register(c => new IdempotentBus(c.Resolve<IBus>(), c.Resolve<ITrackUnitOfWork>()))
-				.As<IIdempotentBus>()
-				.ContainerScoped()
-				.ExternallyOwned();
-
-			builder
-				.Register(c => new MessageDispatcher(c.Resolve<IBus>()))
-				.As<IDispatchMessages>()
-				.ContainerScoped()
-				.ExternallyOwned();
-
-			builder
-				.Register(c => new MessageBuilder(DateTime.UtcNow))
-				.As<IBuildMessages>()
-				.As<IRegisterMessageIdentifiers>()
-				.ContainerScoped()
-				.ExternallyOwned();
 
 			builder
 				.Register(c => new TestMessageHandler())
--- MyConsumer/EndpointConfig.cs
+++ MyConsumer/EndpointConfig.cs
@@ -1,17 +1,17 @@
 namespace MyConsumer
 {
 	using Autofac.Builder;
+	using IdempotentConsumer.Autofac;
 	using NServiceBus;
 	using NServiceBus.MessageSinks.AutofacConfiguration;
 
-	public class EndpointConfig : IConfigureThisEndpoint,
-		AsA_Server,
-		IWantCustomInitialization
+	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
 	{
 		public void Init()
 		{
 			var builder = new ContainerBuilder();
 			builder.RegisterModule(new MessageSinkConfigurationModule());
+			builder.RegisterModule(new IdempotentConfigurationModule());
 			builder.RegisterModule(new ConfigModule());
 
 			Configure.With()
